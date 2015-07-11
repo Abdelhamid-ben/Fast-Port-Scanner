9ccb9b5e701127ac5b3f6ad4ccf85a99abbd40ae
Mid Set <rooter.dz@gmail.com>
2015-07-11 21:32:15 +0000

    Create pyscan.py

---
+++ pyscan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#Copyright to Mid Set
+#Contact me : fb.com/midset00
+import sys
+import socket
+import socket
+import time
+import os
+from socket import socket
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+s=socket.socket(socket.AF_INET, socket.SOCK_stream)
+data=s.recv(1024)
+try:
+ s.connect((host,port))
+except socket.error :
+ print ('port is close')
+ sys.exit()
+time.sleep(2)
+print ("trying")
+time.sleep(1)
+print ('host is alive')
+print ('port is open')
